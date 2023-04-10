807b76e1eb040f236dbb42042714a7d67ee8d569
Jorge Gonzalez <jorgeegonzalezd@gmail.com>
2023-04-09 22:31:53 -0500

    Commit inicial

---
+++ 03-format strings.py
@@ -0,0 +1,9 @@
+nombre = "Jorge"
+apellido = "Gonzalez"
+nombre_completo = f"{nombre} {apellido}"
+print(nombre_completo)
+
+animal = " peggy cienta "
+print(animal.upper())
+print(animal.title())
+print(animal.strip())
